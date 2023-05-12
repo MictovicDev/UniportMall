cd55bb795294049e4d0830a3ab985707427f1e40
Emeka <awaemekamichael@gmail.com>
2023-05-12 19:19:27 +0100

    staticfiles

--- mictovic/settings.py
+++ mictovic/settings.py
@@ -221,9 +221,12 @@ else:
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
 STATIC_URL = 'static/'
+
 STATICFILES_DIRS = [
 BASE_DIR / 'static',
 ]
+
+STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
 
 # STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
